bcefc2dba6f224d7fd46bceba3183b08315d1b30
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-02-18 08:33:24 +0530

    Add files via upload

---
+++ p20.py
@@ -0,0 +1,15 @@
+#for with else
+x=1
+for i in range(x,6):
+    if(x<5):
+        print(i)
+else:
+    print("Nor greater than 10")
+
+#while with else
+count=0
+while count<5:
+    print(count," is less than")
+    count = count+1
+else:
+    print(count," is not less than 5")
---
+++ p22.py
@@ -0,0 +1,24 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print("\n")
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print("\n")
+    
+'''
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+'''
+    
