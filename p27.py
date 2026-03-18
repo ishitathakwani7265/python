f70548d9f6af9906b493ca1e471754fd818396ff
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-03-18 08:33:24 +0530

    Add files via upload

---
+++ p27.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+a = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="college"
+)
+
+mycursor = a.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
